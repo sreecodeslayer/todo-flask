2156f7c0b045c5c4c3fb035485dd44854776c464
Sreenadh T C <kesav.tc8@gmail.com>
2016-12-30 20:07:50 -0400

    Add routes for login,signup and logout in backend

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask, jsonify, render_template, url_for
+from flask import Flask, jsonify, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/login", methods=['POST'])
+def login():
+	params = request.get_json()
+	print params
+	return jsonify({'status':True})
+
+@app.route("/signup", methods=['POST'])
+def signup():
+	params = request.get_json()
+	print params
+	return jsonify({'status':True})
+
+@app.route("/logout", methods=['POST'])
+def logout():
+	return True
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True, host='0.0.0.0')
