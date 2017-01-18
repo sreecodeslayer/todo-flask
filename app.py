5f895e0f63c06a695faeabe23f4c172bc50a5912
Sreenadh T C <kesav.tc8@gmail.com>
2017-01-18 23:45:50 +0530

    Redirect to index.html on source Route

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify({'message':'Backend is up!'})
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True, host='0.0.0.0')
