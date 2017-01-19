5d1a599db24b248e1f808c378683bd7bcc4645bd
Sreenadh T C <kesav.tc8@gmail.com>
2017-01-19 22:27:45 +0530

    Add login function to request to server

--- static/js/app.js
+++ static/js/app.js
@@ -3,5 +3,17 @@ var tApp = angular.module('todoApp', ['ngRoute'],function($interpolateProvider){
     $interpolateProvider.endSymbol(']]');
 });
 tApp.controller('TodoController', ['$scope', '$http', '$location', '$route', function($scope, $http, $location, $route) {
-
+	$scope.login = function(){
+		$http({
+			url: '/login',
+			method: 'POST',
+			data: JSON.stringify({
+				'username':$scope.login.username,
+				'password':$scope.login.password
+			}),
+			headers: {'Content-Type':'application/json'}
+		}).then(function(response){
+			console.log(response['status'] + ' ' + response['statusText']);
+		});
+	}
 }]);
