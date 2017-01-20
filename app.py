12474630139eccbb8c257077ae323f88b6c6160b
Sreenadh T C <kesav.tc8@gmail.com>
2017-01-20 20:49:35 +0530

    Add basic new task function

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, jsonify, render_template, url_for, request
 from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user, current_user
 from flask_mongoengine import MongoEngine, DoesNotExist
 from settings import db,app
-from models import User
+from models import User, Task
 import bcrypt
+from uuid import uuid4
 
 # now get all the models from models.py ( this can be inside app.py)
 # am just making it in models.py for readbility
@@ -71,7 +72,28 @@ def logout():
 def new_task():
 	data = request.get_json()
 	# Add new task for the user
-	return jsonify({'status':True})
+	print data
+	try:
+		task_obj = Task()
+		task_obj.user = User.objects.get(id=current_user.id)
+		task_obj.task_title = data['task_title']
+		task_obj.task_content = data['task_content']
+		task_obj.task_id = str(uuid4())
+		task_obj.save()
+
+		# Now get all task for this user and return it
+		all_tasks = Task.objects(user = current_user.id)
+		his_tasks = []
+		for task in all_tasks:
+			his_tasks.append({
+				'task_title':task.task_title,
+				'task_content':task.task_content,
+				'task_id':task.task_id
+				})
+		return jsonify({'status':True,'tasks':his_tasks})
+	except Exception as e:
+		print e
+		return jsonify({'status':False})
 
 # UPDATE
 @app.route("/edit", methods=['POST'])
